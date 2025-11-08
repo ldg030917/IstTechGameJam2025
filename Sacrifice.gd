cbeaeecf0e849338de609a1908e2970c8c585295
ldg030917 <127109929+ldg030917@users.noreply.github.com>
2025-11-08 17:57:38 +0900

    FEAT: sacrifice dead motion

--- Sacrifice.gd
+++ Sacrifice.gd
@@ -2,16 +2,23 @@ extends CharacterBody2D
 
 class_name Sacrifice
 
+@onready var wander_timer = $Wander
+@onready var animation_player = $AnimationPlayer
+
 @export var speed: float = 100
-@export var hp: float = 10
+@export var hp: float = 5
 @export var Dgg: float = 0.1 # Delta god gauge
-@export var attack: float = 10
+@export var attack: float = 1
 var state: State = State.IDLE
 var target: Node2D
 
 enum Type { HOSTILE, NEUTRAL, FRIENDLY }
 enum State { IDLE, CHASE, ATTACK, STUN, DEAD }
 
+func _ready() -> void:
+	state = State.IDLE
+	$Sprite2D/Label.text = str(hp)
+	pass
 
 func _physics_process(delta: float) -> void:
 	match state:
@@ -21,11 +28,9 @@ func _physics_process(delta: float) -> void:
 		State.CHASE:
 			var direction = global_position.direction_to(target.global_position)
 			velocity = direction * speed
-	
+		_:
+			velocity = Vector2.ZERO
 	move_and_slide()
-
-
-
 
 func _on_sight_area_body_entered(body: Node2D) -> void:
 	if body is Player:
@@ -40,8 +45,13 @@ func _on_chasing_area_body_exited(body: Node2D) -> void:
 
 func hurt(damage: int):
 	hp -= damage
+	$Sprite2D/Label.text = str(hp)
 	#print("hurt")
 	$Sprite2D.self_modulate = Color.RED
 	if hp <= 0:
 		state = State.DEAD
+		animation_player.play("death")
 		self_modulate.a = 0.5
+
+func _on_wander_timeout() -> void:
+	pass # Replace with function body.
